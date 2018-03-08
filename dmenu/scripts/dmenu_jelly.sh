627be06be21b5099f184f2af3f5593c17736c3da
Daniel Rivas <ers.daniel+dev@gmail.com>
2018-03-08 16:37:08 -0600

    Cleaned up some files

--- dmenu/scripts/dmenu_jelly.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-. ~/.config/colors/colorscheme.sh
-
-dmenu_run -b -l 4 -fn "xft:Source Code Pro:medium:pixelsize=16" -nb $COLOR_BG -nf $COLOR_FG -sb $COLOR_YELLOW -sf $COLOR_BLACK
--- leiningen/profiles.clj
+++
@@ -1,1 +0,0 @@
-{:user {:plugins [[cider/cider-nrepl "0.11.0"]]}}
