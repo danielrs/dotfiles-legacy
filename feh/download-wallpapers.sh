ad307c170df25a1bf1376722c9db14818cca8b5e
Daniel Rivas <ers.daniel+dev@gmail.com>
2018-06-16 22:51:04 -0500

    Added check for background downloader

--- feh/download-wallpapers.sh
+++ feh/download-wallpapers.sh
@@ -7,5 +7,7 @@ declare -a wallpapers=("wallhaven-655368.jpg" "wallhaven-662258.jpg")
 
 mkdir -p "${INSTALL_DIR}"
 for id in "${wallpapers[@]}"; do
-    curl "${BASE_URL}${id}" > "${INSTALL_DIR}${id}"
+    if [ ! -f ${INSTALL_DIR}${id} ]; then
+        curl "${BASE_URL}${id}" > "${INSTALL_DIR}${id}"
+    fi
 done
