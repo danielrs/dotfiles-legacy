98389412951f9029cae9920dd6c05cc3087432b4
Daniel Rivas <ers.daniel+dev@gmail.com>
2018-06-16 22:48:58 -0500

    Updated wallpapers

--- feh/download-wallpapers.sh
+++ feh/download-wallpapers.sh
@@ -3,7 +3,7 @@
 INSTALL_DIR="$HOME/Pictures/Wallpapers/feh/"
 
 BASE_URL="https://wallpapers.wallhaven.cc/wallpapers/full/"
-declare -a wallpapers=("wallhaven-655368.jpg")
+declare -a wallpapers=("wallhaven-655368.jpg" "wallhaven-662258.jpg")
 
 mkdir -p "${INSTALL_DIR}"
 for id in "${wallpapers[@]}"; do
